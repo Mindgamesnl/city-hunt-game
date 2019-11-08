e69df48244d7ad718d6e98eccfe950fb2a94d5d2
mindgamesnl pc <matsmoolhuizen@gmail.com>
2019-11-08 13:51:38 +0100

    fixiwixy

--- backend/modules/rest/RestService.js
+++ backend/modules/rest/RestService.js
@@ -9,7 +9,17 @@ module.exports.RestService = class RestService {
     constructor() {
 
         // setup SSL
-        let app = require('express')();
+        this.app = require('express')();
+
+        const bodyParser = require('body-parser');
+        const cors = require('cors');
+
+        this.app.use( bodyParser.json() );
+        this.app.use(cors());
+        this.app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+            extended: true
+        }));
+
         let https = require('https');
 
         let options = {
@@ -17,20 +27,11 @@ module.exports.RestService = class RestService {
             cert: fs.readFileSync(config.config.cert.cert)
         };
 
-        this.http = https.createServer(options, app);
+        this.http = https.createServer(options, this.app);
 
         this.http.listen(config.config.port);
 
         this.endpoints = 0;
-
-        const bodyParser = require('body-parser');
-        const cors = require('cors');
-
-        this.http.use( bodyParser.json() );
-        this.http.use(cors());
-        this.http.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-            extended: true
-        }));
     }
 
     boot() {
